695e289b29171027bc8671b6ef3d65f4b18ea34e
osmancorluk <corluk@hotmail.com>
2021-02-13 12:54:23 +0300

    typescript definition file added

---
+++ index.d.ts
@@ -0,0 +1,19 @@
+declare module "number-to-text" {
+
+    /***
+     * 
+    num - a string or number.
+    options
+        language - Default: en-us - possible Values : en-us, 'en-in', 'de', 'tr'
+        separator - Default: , for (en-us and en-in)
+        case - Default: titleCase - possible Values : "titleCase" , "lowerCase" , "upperCase" for (en-us and en-in)
+
+     */
+
+    interface  NumberToTextOptions  {
+        language: string , 
+        separator : string , 
+        case : string  
+    } 
+    function convert(num : number , options : NumberToTextOptions)
+}
